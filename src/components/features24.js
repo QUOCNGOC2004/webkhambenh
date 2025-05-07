bf4a0f6e4f5419f9d528dc977145ec229141a1d5
QUOCNGOC2004 <nguyenngocvt04@gmail.com>
2025-05-07 11:28:50 +0700

    newProjectnewProject

---
+++ src/components/features24.js
@@ -0,0 +1,157 @@
+import React, { useState, Fragment } from 'react'
+
+import PropTypes from 'prop-types'
+
+import './features24.css'
+
+const Features24 = (props) => {
+  const [activeTab, setActiveTab] = useState(0)
+  return (
+    <div className="thq-section-padding">
+      <div className="features24-container2 thq-section-max-width">
+        <div className="features24-image-container">
+          {activeTab === 0 && (
+            <img
+              alt={props.feature1ImgAlt}
+              src={props.feature1ImgSrc}
+              className="features24-image1 thq-img-ratio-16-9"
+            />
+          )}
+          {activeTab === 1 && (
+            <img
+              alt={props.feature2ImgAlt}
+              src={props.feature2ImgSrc}
+              className="features24-image2 thq-img-ratio-16-9"
+            />
+          )}
+          {activeTab === 2 && (
+            <img
+              alt={props.feature3ImgAlt}
+              src={props.feature3ImgSrc}
+              className="features24-image3 thq-img-ratio-16-9"
+            />
+          )}
+        </div>
+        <div className="features24-tabs-menu">
+          <div
+            onClick={() => setActiveTab(0)}
+            className="features24-tab-horizontal1"
+          >
+            <div className="features24-divider-container1">
+              {activeTab === 0 && <div className="features24-container3"></div>}
+            </div>
+            <div className="features24-content1">
+              <h2 className="thq-heading-2">
+                {props.feature1Title ?? (
+                  <Fragment>
+                    <span className="features24-text4">
+                      User-friendly Interface
+                    </span>
+                  </Fragment>
+                )}
+              </h2>
+              <span className="thq-body-small">
+                {props.feature1Description ?? (
+                  <Fragment>
+                    <span className="features24-text5">
+                      Modern and responsive design for seamless user experience
+                    </span>
+                  </Fragment>
+                )}
+              </span>
+            </div>
+          </div>
+          <div
+            onClick={() => setActiveTab(1)}
+            className="features24-tab-horizontal2"
+          >
+            <div className="features24-divider-container2">
+              {activeTab === 1 && <div className="features24-container4"></div>}
+            </div>
+            <div className="features24-content2">
+              <h2 className="thq-heading-2">
+                {props.feature2Title ?? (
+                  <Fragment>
+                    <span className="features24-text6">Find Doctors</span>
+                  </Fragment>
+                )}
+              </h2>
+              <span className="thq-body-small">
+                {props.feature2Description ?? (
+                  <Fragment>
+                    <span className="features24-text3">
+                      Explore a list of doctors with filters by specialty and
+                      availability
+                    </span>
+                  </Fragment>
+                )}
+              </span>
+            </div>
+          </div>
+          <div
+            onClick={() => setActiveTab(2)}
+            className="features24-tab-horizontal3"
+          >
+            <div className="features24-divider-container3">
+              {activeTab === 2 && <div className="features24-container5"></div>}
+            </div>
+            <div className="features24-content3">
+              <h2 className="thq-heading-2">
+                {props.feature3Title ?? (
+                  <Fragment>
+                    <span className="features24-text2">Book Appointments</span>
+                  </Fragment>
+                )}
+              </h2>
+              <span className="thq-body-small">
+                {props.feature3Description ?? (
+                  <Fragment>
+                    <span className="features24-text1">
+                      View doctor&apos;s availability and schedule appointments
+                      easily
+                    </span>
+                  </Fragment>
+                )}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Features24.defaultProps = {
+  feature1ImgAlt: 'Homepage image',
+  feature3Description: undefined,
+  feature3Title: undefined,
+  feature3ImgSrc:
+    'https://images.unsplash.com/photo-1626772959824-8786482c6fcf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY3NXw&ixlib=rb-4.0.3&q=80&w=1080',
+  feature1ImgSrc:
+    'https://images.unsplash.com/photo-1617103199187-aff5bebc77ee?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY3Nnw&ixlib=rb-4.0.3&q=80&w=1080',
+  feature2Description: undefined,
+  feature1Title: undefined,
+  feature3ImgAlt: 'Book Appointment image',
+  feature1Description: undefined,
+  feature2ImgSrc:
+    'https://images.unsplash.com/photo-1689289270523-73a662b84f74?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY3N3w&ixlib=rb-4.0.3&q=80&w=1080',
+  feature2ImgAlt: 'List of Doctors image',
+  feature2Title: undefined,
+}
+
+Features24.propTypes = {
+  feature1ImgAlt: PropTypes.string,
+  feature3Description: PropTypes.element,
+  feature3Title: PropTypes.element,
+  feature3ImgSrc: PropTypes.string,
+  feature1ImgSrc: PropTypes.string,
+  feature2Description: PropTypes.element,
+  feature1Title: PropTypes.element,
+  feature3ImgAlt: PropTypes.string,
+  feature1Description: PropTypes.element,
+  feature2ImgSrc: PropTypes.string,
+  feature2ImgAlt: PropTypes.string,
+  feature2Title: PropTypes.element,
+}
+
+export default Features24
---
+++ src/components/hero17.js
@@ -0,0 +1,293 @@
+import React, { Fragment } from 'react'
+
+import Script from 'dangerous-html/react'
+import PropTypes from 'prop-types'
+
+import './hero17.css'
+
+const Hero17 = (props) => {
+  return (
+    <div className="hero17-header78">
+      <div className="hero17-column thq-section-padding thq-section-max-width">
+        <div className="hero17-content1">
+          <h1 className="hero17-text1 thq-heading-1">
+            {props.heading1 ?? (
+              <Fragment>
+                <span className="hero17-text7">
+                  Welcome to our Online Booking System
+                </span>
+              </Fragment>
+            )}
+          </h1>
+          <p className="hero17-text2 thq-body-large">
+            {props.content1 ?? (
+              <Fragment>
+                <span className="hero17-text8">
+                  Easily schedule your medical appointments with just a few
+                  clicks. Find the right doctor for your needs and manage your
+                  appointments hassle-free.
+                </span>
+              </Fragment>
+            )}
+          </p>
+        </div>
+        <div className="hero17-actions">
+          <button className="thq-button-filled hero17-button1">
+            <span className="thq-body-small">
+              {props.action1 ?? (
+                <Fragment>
+                  <span className="hero17-text6">Get Started</span>
+                </Fragment>
+              )}
+            </span>
+          </button>
+          <button className="thq-button-outline hero17-button2">
+            <span className="thq-body-small">
+              {props.action2 ?? (
+                <Fragment>
+                  <span className="hero17-text5">Secondary action</span>
+                </Fragment>
+              )}
+            </span>
+          </button>
+        </div>
+      </div>
+      <div className="hero17-content2">
+        <div className="hero17-row-container1 thq-animated-group-container-horizontal thq-mask-image-horizontal">
+          <div className="thq-animated-group-horizontal">
+            <img
+              alt={props.image1Alt}
+              src={props.image1Src}
+              className="hero17-placeholder-image10 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image2Alt}
+              src={props.image2Src}
+              className="hero17-placeholder-image11 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image3Alt}
+              src={props.image3Src}
+              className="hero17-placeholder-image12 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image4Alt}
+              src={props.image4Src}
+              className="hero17-placeholder-image13 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image5Alt}
+              src={props.image5Src}
+              className="hero17-placeholder-image14 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image6Alt}
+              src={props.image6Src}
+              className="hero17-placeholder-image15 thq-img-scale thq-img-ratio-1-1"
+            />
+          </div>
+          <div className="thq-animated-group-horizontal">
+            <img
+              alt={props.image1Alt}
+              src={props.image1Src}
+              className="hero17-placeholder-image16 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image2Alt}
+              src={props.image2Src}
+              className="hero17-placeholder-image17 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image3Alt}
+              src={props.image3Src}
+              className="hero17-placeholder-image18 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image4Alt}
+              src={props.image4Src}
+              className="hero17-placeholder-image19 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image5Alt}
+              src={props.image5Src}
+              className="hero17-placeholder-image20 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt="Hero Image"
+              src="https://images.unsplash.com/photo-1534312527009-56c7016453e6?ixid=M3w5MTMyMXwwfDF8c2VhcmNofDIxfHxhYnN0cmFjdHxlbnwwfHx8fDE3MTA4NzA5MzB8MA&amp;ixlib=rb-4.0.3&amp;w=1500"
+              className="hero17-placeholder-image21 thq-img-scale thq-img-ratio-1-1"
+            />
+          </div>
+        </div>
+        <div className="hero17-row-container2 thq-animated-group-container-horizontal thq-mask-image-horizontal">
+          <div className="thq-animated-group-horizontal-reverse">
+            <img
+              alt={props.image7Alt}
+              src={props.image7Src}
+              className="hero17-placeholder-image22 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image8Alt}
+              src={props.image8Src}
+              className="hero17-placeholder-image23 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image9Alt}
+              src={props.image9Src}
+              className="hero17-placeholder-image24 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image10Alt}
+              src={props.image10Src}
+              className="hero17-placeholder-image25 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image11Alt}
+              src={props.image11Src}
+              className="hero17-placeholder-image26 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image12Alt}
+              src={props.image12Src}
+              className="hero17-placeholder-image27 thq-img-scale thq-img-ratio-1-1"
+            />
+          </div>
+          <div className="thq-animated-group-horizontal-reverse">
+            <img
+              alt={props.image7Alt}
+              src={props.image7Src}
+              className="hero17-placeholder-image28 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image8Alt}
+              src={props.image8Src}
+              className="hero17-placeholder-image29 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image9Alt}
+              src={props.image9Src}
+              className="hero17-placeholder-image30 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image10Alt}
+              src={props.image10Src}
+              className="hero17-placeholder-image31 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image11Alt}
+              src={props.image11Src}
+              className="hero17-placeholder-image32 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt="Hero Image"
+              src="https://images.unsplash.com/photo-1568214379698-8aeb8c6c6ac8?ixid=M3w5MTMyMXwwfDF8c2VhcmNofDEyfHxncmFmaWN8ZW58MHx8fHwxNzE1Nzk0OTk5fDA&amp;ixlib=rb-4.0.3&amp;w=1500"
+              className="hero17-placeholder-image33 thq-img-scale thq-img-ratio-1-1"
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="hero17-container2">
+          <Script
+            html={`<style>
+  @keyframes scroll-x {
+    from {
+      transform: translateX(0);
+    }
+    to {
+      transform: translateX(calc(-100% - 16px));
+    }
+  }
+
+  @keyframes scroll-y {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(calc(-100% - 16px));
+    }
+  }
+</style>
+`}
+          ></Script>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Hero17.defaultProps = {
+  image3Src:
+    'https://images.unsplash.com/photo-1488229297570-58520851e868?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY3NXw&ixlib=rb-4.0.3&q=80&w=1080',
+  image8Alt: 'Hero Image',
+  image2Src:
+    'https://images.unsplash.com/photo-1644168819398-7d73bc566488?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY3Nnw&ixlib=rb-4.0.3&q=80&w=1080',
+  image6Alt: 'Hero Image',
+  image11Src:
+    'https://images.unsplash.com/photo-1708352547543-26fcdec780af?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY3Nnw&ixlib=rb-4.0.3&q=80&w=1080',
+  image5Alt: 'Hero Image',
+  image1Alt: 'Doctor with patient',
+  image7Src:
+    'https://images.unsplash.com/photo-1533160772606-ba8c6a74d336?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY3N3w&ixlib=rb-4.0.3&q=80&w=1080',
+  image7Alt: 'Hero Image',
+  image12Alt: 'Hero Image',
+  image2Alt: 'Calendar',
+  image6Src:
+    'https://images.unsplash.com/photo-1469571486292-0ba58a3f068b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY3N3w&ixlib=rb-4.0.3&q=80&w=1080',
+  image12Src:
+    'https://images.unsplash.com/photo-1637071076879-f60b5ec73a04?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY3N3w&ixlib=rb-4.0.3&q=80&w=1080',
+  image3Alt: 'Hero Image',
+  image9Src:
+    'https://images.unsplash.com/photo-1716718406028-1229c98d0eba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY3OHw&ixlib=rb-4.0.3&q=80&w=1080',
+  image11Alt: 'Hero Image',
+  action2: undefined,
+  action1: undefined,
+  image8Src:
+    'https://images.unsplash.com/photo-1634692843550-8ddff2a880ad?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY3OHw&ixlib=rb-4.0.3&q=80&w=1080',
+  image5Src:
+    'https://images.unsplash.com/photo-1541462608143-67571c6738dd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY4MHw&ixlib=rb-4.0.3&q=80&w=1080',
+  image4Src:
+    'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY4MHw&ixlib=rb-4.0.3&q=80&w=1080',
+  image10Alt: 'Hero Image',
+  image4Alt: 'Hero Image',
+  heading1: undefined,
+  content1: undefined,
+  image10Src:
+    'https://images.unsplash.com/photo-1515954487644-3640503f6594?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY4MHw&ixlib=rb-4.0.3&q=80&w=1080',
+  image9Alt: 'Hero Image',
+  image1Src:
+    'https://images.unsplash.com/photo-1636536319140-9e9004277dc0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTc0NjM3OTY4MXw&ixlib=rb-4.0.3&q=80&w=1080',
+}
+
+Hero17.propTypes = {
+  image3Src: PropTypes.string,
+  image8Alt: PropTypes.string,
+  image2Src: PropTypes.string,
+  image6Alt: PropTypes.string,
+  image11Src: PropTypes.string,
+  image5Alt: PropTypes.string,
+  image1Alt: PropTypes.string,
+  image7Src: PropTypes.string,
+  image7Alt: PropTypes.string,
+  image12Alt: PropTypes.string,
+  image2Alt: PropTypes.string,
+  image6Src: PropTypes.string,
+  image12Src: PropTypes.string,
+  image3Alt: PropTypes.string,
+  image9Src: PropTypes.string,
+  image11Alt: PropTypes.string,
+  action2: PropTypes.element,
+  action1: PropTypes.element,
+  image8Src: PropTypes.string,
+  image5Src: PropTypes.string,
+  image4Src: PropTypes.string,
+  image10Alt: PropTypes.string,
+  image4Alt: PropTypes.string,
+  heading1: PropTypes.element,
+  content1: PropTypes.element,
+  image10Src: PropTypes.string,
+  image9Alt: PropTypes.string,
+  image1Src: PropTypes.string,
+}
+
+export default Hero17
