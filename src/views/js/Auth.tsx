7b0fa9d6518d7ab163e328cca4767c736f275f39
QUOCNGOC2004 <nguyenngocvt04@gmail.com>
2025-05-09 02:55:37 +0700

    login

--- src/views/js/Auth.tsx
+++ src/views/js/Auth.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../css/Auth.css'; 
-import 'boxicons/css/boxicons.min.css'; // Import Boxicons CSS
+import 'boxicons/css/boxicons.min.css'; 
 
 const Auth: React.FC = () => {
     const [isActive, setIsActive] = useState(false);
@@ -22,20 +22,20 @@ const Auth: React.FC = () => {
         <div className={containerClass}>
             <div className="form-box login">
                 <form action="#">
-                    <h1>Login</h1>
+                    <h1>Đăng nhập</h1>
                     <div className="input-box">
-                        <input type="text" placeholder="Username" required />
+                        <input type="text" placeholder="Tên đăng nhập" required />
                         <i className='bx bxs-user'></i>
                     </div>
                     <div className="input-box">
-                        <input type="password" placeholder="Password" required />
+                        <input type="password" placeholder="Mật khẩu" required />
                         <i className='bx bxs-lock-alt' ></i>
                     </div>
                     <div className="forgot-link">
-                        <a href="#">Forgot Password?</a>
+                        <a href="#">Quên mật khẩu?</a>
                     </div>
-                    <button type="submit" className="btn">Login</button>
-                    <p>or login with social platforms</p>
+                    <button type="submit" className="btn">Đăng nhập</button>
+                    <p>hoặc đăng nhập bằng</p>
                     <div className="social-icons">
                         <a href="#"><i className='bx bxl-google' ></i></a>
                         <a href="#"><i className='bx bxl-facebook' ></i></a>
@@ -47,9 +47,9 @@ const Auth: React.FC = () => {
 
             <div className="form-box register">
                 <form action="#">
-                    <h1>Registration</h1>
+                    <h1>Đăng ký</h1>
                     <div className="input-box">
-                        <input type="text" placeholder="Username" required />
+                        <input type="text" placeholder="Tên đăng nhập" required />
                         <i className='bx bxs-user'></i>
                     </div>
                     <div className="input-box">
@@ -57,11 +57,11 @@ const Auth: React.FC = () => {
                         <i className='bx bxs-envelope' ></i>
                     </div>
                     <div className="input-box">
-                        <input type="password" placeholder="Password" required />
+                        <input type="password" placeholder="Mật khẩu" required />
                         <i className='bx bxs-lock-alt' ></i>
                     </div>
-                    <button type="submit" className="btn">Register</button>
-                    <p>or register with social platforms</p>
+                    <button type="submit" className="btn">Đăng ký</button>
+                    <p>hoặc đăng ký bằng</p>
                     <div className="social-icons">
                         <a href="#"><i className='bx bxl-google' ></i></a>
                         <a href="#"><i className='bx bxl-facebook' ></i></a>
@@ -73,15 +73,15 @@ const Auth: React.FC = () => {
 
             <div className="toggle-box">
                 <div className="toggle-panel toggle-left">
-                    <h1>Hello, Welcome!</h1>
-                    <p>Don't have an account?</p>
-                    <button className="btn register-btn" onClick={handleRegisterClick}>Register</button>
+                    <h1>Chào mừng trở lại!</h1>
+                    <p>Bạn chưa có tài khoản?</p>
+                    <button className="btn register-btn" onClick={handleRegisterClick}>Đăng ký</button>
                 </div>
 
                 <div className="toggle-panel toggle-right">
-                    <h1>Welcome Back!</h1>
-                    <p>Already have an account?</p>
-                    <button className="btn login-btn" onClick={handleLoginClick}>Login</button>
+                    <h1>Xin chào!</h1>
+                    <p>Bạn đã có tài khoản?</p>
+                    <button className="btn login-btn" onClick={handleLoginClick}>Đăng nhập</button>
                 </div>
             </div>
         </div>
